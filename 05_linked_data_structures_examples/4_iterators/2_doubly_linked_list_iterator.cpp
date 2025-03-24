9f7bf62f16da6eb0139ff054d7dbf91f64c93162
IamSDAM <avila.sergiom05@gmail.com>
2025-03-24 03:44:28 +0000

    updated

--- 05_linked_data_structures_examples/4_iterators/2_doubly_linked_list_iterator.cpp
+++ 05_linked_data_structures_examples/4_iterators/2_doubly_linked_list_iterator.cpp
@@ -307,6 +307,47 @@ int main() {
 
     std::cout << std::endl;
     return 0;
+    /*
+    #include <iostream>
+
+class Course {
+private:
+    std::string number;
+public:
+    Course(): number("101") 
+    {}
+    std::string getNumber()
+    {
+        return number;
+    }
+    void setNumber(std::string n)
+    {
+        number = n;
+    }
+    std::string getProfName() 
+    {
+        return prof.name;
+    }
+    void setProfName(std::string n)
+    {
+        prof.name = n;
+    }
+};
+void print(Course &c) 
+{
+
+}
+
+int main() {
+    std::cout << std::endl;
+
+    Course c;
+    c.setNumber("580");
+    c.print();
+
+    std::cout << std::endl;
+    return 0;
+}*/
 }
 
 
